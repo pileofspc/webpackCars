6157314dd04375eabfaa9d16efe5e2917c83b2df
Александр Макаров <alexmk.work@gmail.com>
2023-01-19 19:20:42 +0500

    поменял пути на относительные

--- src/js/misc.js
+++ src/js/misc.js
@@ -1,8 +1,8 @@
 import global from './global'
 
 // Notification bell
-let unreadBellPath = '/assets/img/notification-bell_active.svg';
-let bellPath = '/assets/img/notification-bell.svg';
+let unreadBellPath = 'assets/img/notification-bell_active.svg';
+let bellPath = 'assets/img/notification-bell.svg';
 
 if (global.database.unreadNotifications) {
     let bell = document.querySelector('.notification-bell__img');
