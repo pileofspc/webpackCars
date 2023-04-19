a83a97fdee4a115e37750999ae97f3e3a11a1728
Александр Макаров <alexmk.work@gmail.com>
2023-04-19 19:52:52 +0500

    микрофикс

--- src/js/test.js
+++ src/js/test.js
@@ -1,5 +1,5 @@
-// import 'modules/ProductCard/ProductCard.scss';
-import Header from 'modules/Header/Header';
+// import '@modules/ProductCard/ProductCard.scss';
+import Header from '@modules/Header/Header';
 
 
 // document.body.append(new Header().mainNode);
